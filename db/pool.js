6beb9628b38addc8c45be51ce2858b182f402664
Okekunle Opeyemi <icepeakb@gmail.com>
2025-05-29 18:28:21 +0100

    db url

--- db/pool.js
+++ db/pool.js
@@ -2,15 +2,9 @@
 const { Pool } = require('pg');
 const { db } = require('../config');
 
-const pool = new Pool({
-  host: db.host,
-  port: db.port,
-  user: db.user,
-  password: db.pass,
-  database: db.name,
-  max: 20,            // adjust pool size as needed
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-});
+
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
+
 
 module.exports = pool;
