90947461969316138f9d24ce1e08be328ec8aa8f
Okekunle Opeyemi <icepeakb@gmail.com>
2025-05-29 19:23:05 +0100

    Update pool.js

--- db/pool.js
+++ db/pool.js
@@ -1,18 +1,20 @@
-// src/db/pool.js
+// db/pool.js
 const { Pool } = require('pg');
-const { db } = require('../config');
+require('dotenv').config();
 
 const pool = new Pool({
-  host: db.host,
-  port: db.port,
-  user: db.user,
-  password: db.pass,
-  database: db.name,
-  max: 20,            // adjust pool size as needed
+  host:     process.env.DB_HOST,
+  port:     +process.env.DB_PORT,
+  user:     process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
+  ssl: {
+    // Allow self-signed certs (Render uses these)
+    rejectUnauthorized: false
+  }
 });
-
-
 
 module.exports = pool;
